57395871a580d9c7aa97a597ae3dd792bb5939a0
Willie Wrinkle <williewrinkle@gmail.com>
2018-02-07 22:26:37 -0600

    first commit

---
+++ angular_django_boilerplate/angular/angular-django-boilerplate/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, isDevMode } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-django-boilerplate';
+
+  ngOnInit() {
+    console.log(isDevMode());
+  }
+}
